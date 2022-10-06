101f31df46ce5ce32fc586d80a768f3bd58ce893
LuukNoverraz <luuk.noverraz@coderclass.nl>
2022-10-06 14:02:38 +0200

    Reformat large png images to webp

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 const taglines = ['This place is not a place of honor', 'No highly esteemed deed is commemorated here', 'Nothing valued is here', 'What is here was dangerous and repulsive to us',
                     'The danger is still present, in your time, as it was in ours', 'The danger is to the body, and it can kill']
 
-const images = ["images/bg/00_spikefield.png", "images/bg/01_spikefield.png", "images/bg/02_spikefield.png", "images/bg/03_landscapeofthorns.png",
-                "images/bg/04_landscapeofthorns.png", "images/bg/05_forbiddingblocks.png"]
+const images = ["images/bg/00_spikefield.webp", "images/bg/01_spikefield.webp", "images/bg/02_spikefield.webp", "images/bg/03_landscapeofthorns.webp",
+                "images/bg/04_landscapeofthorns.webp", "images/bg/05_forbiddingblocks.webp"]
 
 
 document.getElementsByClassName("landing")[0].style.background = "url(" + images[Math.floor(Math.random() * images.length)] + ") no-repeat center";
